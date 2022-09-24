362afcccf73258b194c0d3844246aa4a2677826c
sefzaa <2011521016_sefza@student.unand.ac.id>
2022-09-24 18:12:40 +0700

    komat kamit komit

---
+++ app/src/main/java/com/example/myapplication/cardViewMahasiswaSidang.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardViewMahasiswaSidang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view_mahasiswa_sidang);
+    }
+}
