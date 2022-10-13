62025195ede5aae3f85a4c0ebf364e8ad331bec2
deanfisabilandwi1010 <90377775+deanfisabilandwi1010@users.noreply.github.com>
2022-10-13 12:57:13 +0700

    Binding login bismillah

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -6,16 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class login extends AppCompatActivity {
+
+    TextInputEditText editUsername, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
     }
 
     public void login(View view){
         Intent ceklogin = new Intent(login. this,MainActivity.class);
+        String username  = editUsername.getText().toString();
+        String password = editPassword.getText().toString();
+        ceklogin.putExtra("USERNAME", username);
+        ceklogin.putExtra("password", password);
+        ceklogin.putExtra("ISLOGGEDIN", true);
         startActivity(ceklogin);
     }
 }
