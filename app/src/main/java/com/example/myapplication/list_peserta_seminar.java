a01f71b462cd63164683a34072b713001556c890
Mhdlhafiz <muhammadhafiz11133@gmail.com>
2022-12-26 22:21:30 +0700

    ps error di click

--- app/src/main/java/com/example/myapplication/list_peserta_seminar.java
+++ app/src/main/java/com/example/myapplication/list_peserta_seminar.java
@@ -3,12 +3,79 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class list_peserta_seminar extends AppCompatActivity {
+
+import com.example.myapplication.Adapter.pesertaseminarAdapter;
+import com.example.myapplication.models.logbook;
+import com.example.myapplication.models.pesertaseminar;
+
+
+import java.util.ArrayList;
+
+public class list_peserta_seminar extends AppCompatActivity implements pesertaseminarAdapter.ItempsClickListener {
+    private RecyclerView rvps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_peserta_seminar);
+
+        rvps = findViewById(R.id.rv_ps);
+
+        pesertaseminarAdapter adapter = new pesertaseminarAdapter(getpesertaseminar());
+       // adapter.setListener((pesertaseminarAdapter.ItempsClickListener) this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        rvps.setLayoutManager(layoutManager);
+        rvps.setAdapter(adapter);
+    }
+
+    public ArrayList<pesertaseminar> getpesertaseminar() {
+        ArrayList<pesertaseminar> list_ps = new ArrayList<>();
+
+        list_ps.add(new pesertaseminar(
+
+                "Ahmad Fauzan",
+                "1911523020"
+
+        ));
+
+        list_ps.add(new pesertaseminar(
+
+                "Ahmad Fauzan",
+                "1911523020"
+        ));
+
+        list_ps.add(new pesertaseminar(
+
+                "Ahmad Fauzan",
+                "1911523020"
+        ));
+
+        list_ps.add(new pesertaseminar(
+
+                "Ahmad Fauzan",
+                "1911523020"
+        ));
+
+        list_ps.add(new pesertaseminar(
+
+                "Ahmad Fauzan",
+                "1911523020"
+        ));
+
+
+        return list_ps;
+    }
+
+    @Override
+    public void onItemListpesertaseminar(pesertaseminar pesertaseminar) {
+
+    }
+
+    @Override
+    public void onItempsClick(pesertaseminar pesertaseminar) {
     }
 }
